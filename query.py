a38b345e7fefea0143ddabfe14f29968e7beb793
shakilmunavary <shakil.ahamed@gmail.com>
2025-11-09 23:59:44 +0530

    Update query.py

--- query.py
+++ query.py
@@ -1,6 +1,6 @@
 import argparse, os
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import AzureOpenAIEmbeddings
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--plan")
@@ -10,8 +10,15 @@ args = parser.parse_args()
 with open(args.plan) as f:
     query = f.read()
 
-openai_key = os.getenv("OPENAI_API_KEY")
-db = Chroma(collection_name=args.namespace, persist_directory="./chroma", embedding_function=OpenAIEmbeddings(openai_api_key=openai_key))
+embeddings = AzureOpenAIEmbeddings(
+    azure_endpoint=os.getenv("AZURE_API_BASE"),
+    api_key=os.getenv("AZURE_API_KEY"),
+    deployment=os.getenv("DEPLOYMENT_NAME"),
+    model="text-embedding-ada-002",  # or your actual model name
+    api_version=os.getenv("AZURE_API_VERSION")
+)
+
+db = Chroma(collection_name=args.namespace, persist_directory="./chroma", embedding_function=embeddings)
 results = db.similarity_search(query, k=5)
 
 for doc in results:
