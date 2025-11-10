b943573260adb6283e418fb8041ef694908b5b56
shakilmunavary <shakil.ahamed@gmail.com>
2025-11-10 10:16:40 +0530

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -6,6 +6,7 @@ from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--code_dir")
 parser.add_argument("--guardrails")
